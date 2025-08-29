65b081fe3653e3f87604f990baa162557c7ba555
EndWriter <endle.amrane@gmail.com>
2025-08-29 16:21:18 +0200

    add views and change setting for migration

--- db/views.py
+++ db/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import DimType, FactPokemon, PokemonTypes, DimTrainer, PokemonTrainers
+from .serializers import (
+	DimTypeSerializer,
+	FactPokemonSerializer,
+	PokemonTypesSerializer,
+	DimTrainerSerializer,
+	PokemonTrainersSerializer,
+)
 
-# Create your views here.
+class DimTypeViewSet(viewsets.ModelViewSet):
+	queryset = DimType.objects.all()
+	serializer_class = DimTypeSerializer
+
+class FactPokemonViewSet(viewsets.ModelViewSet):
+	queryset = FactPokemon.objects.all()
+	serializer_class = FactPokemonSerializer
+
+class PokemonTypesViewSet(viewsets.ModelViewSet):
+	queryset = PokemonTypes.objects.all()
+	serializer_class = PokemonTypesSerializer
+
+class DimTrainerViewSet(viewsets.ModelViewSet):
+	queryset = DimTrainer.objects.all()
+	serializer_class = DimTrainerSerializer
+
+class PokemonTrainersViewSet(viewsets.ModelViewSet):
+	queryset = PokemonTrainers.objects.all()
+	serializer_class = PokemonTrainersSerializer
